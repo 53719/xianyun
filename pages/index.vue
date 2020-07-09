a61a5d8441acebfb1e37533c204803840d30ba8b
chen1996 <1324417058@qq.com>
2020-07-09 16:23:53 +0800

    设置跳转并且添加less依赖包

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>首页</div>
+  <div>
+    首页
+    <router-link to="/air">机票模块首页</router-link>
+    <router-link to="/hotel">酒店模块首页</router-link>
+    <router-link to="/post">旅游攻略模块</router-link>
+    <router-link to="/user/login">登录/注册</router-link>
+  </div>
 </template>
 
 <script>
