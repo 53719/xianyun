3bfb79b947ef517a7216212b7aba0dfd4a908283
chen1996 <1324417058@qq.com>
2020-07-16 14:51:04 +0800

    注册完毕跳转到登录表单

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -164,6 +164,17 @@ export default {
             data: props
           }).then(res => {
             console.log(res.data);
+            if (res.data.token) {
+              this.$message({
+                message: "注册成功,正在跳转",
+                type: "success"
+              });
+
+              //提示  父组件切换到登录tab
+              setTimeout(() => {
+                this.$emit("tologin");
+              }, 1000);
+            }
           });
         }
       });
